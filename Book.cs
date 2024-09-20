97fe900afbef20156de0123d9c2d556a7f761809
Isaac Akinsanya <IsaacAki247@gmail.com>
2024-09-20 18:54:16 +0100

    Implement Book class with properties, constructor, and basic methods
    - Added properties: Name, Author, ISBNnumber, and AcquisitionDate
    
    - Implemented constructor to initialize the properties
    
    - Overrode ToString method to provide a string representation of the books information
    
    - Added Getters and Setters for all properties

---
+++ Book.cs
@@ -0,0 +1,63 @@
+﻿namespace LibraryRecordSystem
+{
+    class Book
+    {
+        public string Name;
+        public string Author;
+        public int ISBNnumber;
+        public DateTime AcquisitionDate;
+
+        public Book(string name, string author, int isbnNumb, DateTime acquisitionDate)
+        {
+            Name = name;
+            Author = author;
+            ISBNnumber = isbnNumb;
+            AcquisitionDate = acquisitionDate;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} {Author} {ISBNnumber} {AcquisitionDate.ToShortDateString()}";
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public void SetName(string name)
+        {
+            Name = name;
+        }
+
+        public string GetAuthor()
+        {
+            return Author;
+        }
+
+        public void SetAuthor(string author)
+        {
+            Author = author;
+        }
+
+        public int GetISBN()
+        {
+            return ISBNnumber;
+        }
+
+        public void SetISBN(int isbnNumb)
+        {
+            ISBNnumber = isbnNumb;
+        }
+
+        public DateTime GetAcquisitionDate()
+        {
+            return AcquisitionDate;
+        }
+
+        public void SetAcquisitionDate(DateTime acquisitionDate)
+        {
+            AcquisitionDate = acquisitionDate;
+        }
+    }
+}
